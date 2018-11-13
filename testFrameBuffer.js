d89ef65bcd6b67254d75645dd9452a5730a6fcda
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-12 18:05:16 -0600

    added framebuffer display

--- testFrameBuffer.js
+++ testFrameBuffer.js
@@ -1,6 +1,19 @@
 const Color = require("./Color.js");
 const FrameBuffer = require("./FrameBuffer.js");
 
-const frameBuffer = new FrameBuffer(10,10);
-frameBuffer.drawLine(0, 0, 5, 10, new Color(255,255,255));
-frameBuffer.drawCircle(5, 5, 5, new Color(255,255,255));
+const frameBuffer = new FrameBuffer(10, 10);
+frameBuffer.drawLine(0, 0, 4, 9, new Color(200, 0, 0));
+frameBuffer.drawCircle(6, 5, 3, new Color(0, 0, 100));
+
+function toHex(d) {
+  return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+}
+
+for (var y = frameBuffer.height-1; y >= 0; y--) {
+  var row = "";
+  for (var x = 0; x < frameBuffer.width; x++) {
+    const color = frameBuffer.getPixel(x,y);
+    row += toHex(color.r) + " " + toHex(color.g) + " " + toHex(color.b) + "  "
+  }
+  console.log(row);
+}
