2afb391bc7e727b2fc3b6cc69e43b924eaf4869b
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-23 12:09:58 -0600

    added Font

---
+++ Font.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Font {
+  constructor(typeface, size, color) {
+    this.typeface = typeface;
+    this.size = size;
+    this.color = color;
+  }
+
+}
+
+module.exports = Font;
