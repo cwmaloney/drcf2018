3a3b3f3d88e76616ca083aed7b5a81b09937d5f1
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-23 20:23:05 -0600

    backup - WIP

--- MessageScene.js
+++ MessageScene.js
@@ -1,56 +1,80 @@
+const BitmapBuffer = require("./BitmapBuffer.js");
+//const Font = require("./Font.js");
+const Color = require("./Color.js");
+//const colorNameToRgb = require("./config-colors.js");
+
 
 //////////////////////////////////////////////////////////////////////////////
 const RequestQueue = require("./RequestQueue.js");
-//const { Secrets } = require("secrets.js");
-
-// const "Error" = "Error";
-// const okayStatus = "Okay";
 
 class MessageScene {
 
-  constructor(gridzilla, onPaused, nameManager) {
+  constructor(gridzilla, onPaused, nameManager, configuration) {
+    this.gridzilla = gridzilla;
+    this.onPaused = onPaused;
+    this.nameManager = nameManager;
+
+    this.configure(configuration);
+  
     console.log(`loading message queue  @${new Date()} ...`);
-    this.requestQueue = new RequestQueue();
-    this.requestQueue.loadRequests();
+    this.messageQueue = new RequestQueue();
+    this.messageQueue.loadRequests();
     console.log(`loading messages complete  @${new Date()}`);
 
-    this.pause = true;
+    this.paused = true;
     this.onPaused = onPaused;
   }
 
+  configure(configuration) {
+    const {
+      perMesssagePeriod = 5000,
+      period = 60000,
+    } = configuration;
+
+    this.perMesssagePeriod = perMesssagePeriod;
+    this.period = period;
+  }
+
   getRequestCount() {
-    return this.requestQueue.nextId;
+    return this.messageQueue.nextId;
   }
   
-  getActiveMessagesCount() {
-    return this.requestQueue.getActiveMessagesCount();
+  getActiveMessageCount() {
+    return this.messageQueue.getActiveRequests().length;
   }
 
-  getQueuedMessagesCount() {
-    return this.requestQueue.getQueuedMessagesCount();
+  getQueuedMessageCount() {
+    return this.messageQueue.getQueuedRequests().length;
   }
-
 
   //////////////////////////////////////////////////////////////////////////////
   // Scene control 
   //////////////////////////////////////////////////////////////////////////////
-
+  
   run() {
-    this.pause = false;
+    console.log("messageScene run");
+    this.paused = false;
+    this.startTime = Date.now();
+    onTimer(this);
   }
 
   pause() {
-    this.pause = true;
+    console.log("messageScene pause");
+    clearTimeout(this.runningTimer);
+    this.paused = true;
+    this.onPaused();
   }
 
-  abort() {}
-
+  forcePause() {
+    console.log("messageScene forcePause");
+    this.pause();
+  }
 
   //////////////////////////////////////////////////////////////////////////////
   // addMessage 
   //////////////////////////////////////////////////////////////////////////////
 
-   addMessage(request, response) {
+  addMessage(request, response) {
 
     let sender = request.parameters.sender;
     if (sender === undefined || sender == null) {
@@ -64,8 +88,8 @@ class MessageScene {
       return;
     }
    
-    let messageType = request.parameters.messageType;
-    if (messageType === undefined || messageType === null) {
+    let message = request.parameters.message;
+    if (message === undefined || message === null) {
       console.error('grizilla::addMessage - missing messageType');
       return;
     }
@@ -73,32 +97,39 @@ class MessageScene {
     let date = request.parameters.displayDate;
     let time = request.parameters.displayTime;
    
-    console.log(`addMessage: From: ${sender} To: ${recipient} Message: ${messageType} On: ${date} At: ${time}`);
+    console.log(`addMessage: From: ${sender} To: ${recipient} Message: ${message} On: ${date} At: ${time}`);
 
-    const overUseMessage = messageQueue.checkOverUse(request.sessionId);
+    const overUseMessage = this.messageQueue.checkOverUse(request.sessionId);
     if (overUseMessage != null && overUseMessage != undefined) {
       return this.fillResponse(request, response, "Error", overUseMessage);
     }
 
     // check names
-    let senderOkay = nameManager.isNameValid(sender);
+    let senderOkay = this.nameManager.isNameValid(sender);
     if (!senderOkay) {
-      let message = "We do not recognize the sender name - try a common first name.";
-      return this.fillResponse(request, response, "Error", message);
+      let responseMessage = "We do not recognize the sender name - try a common first name.";
+      return this.fillResponse(request, response, "Error", responseMessage);
     }
-    let recipientOkay = nameManager.isNameValid(recipient);
+    let recipientOkay = this.nameManager.isNameValid(recipient);
     if (!recipientOkay) {
-      let message = "We do not recognize the recipient name - try a common first name";
-      return this.fillResponse(request, response, "Error", message);
+      let responseMessage = "We do not recognize the recipient name - try a common first name";
+      return this.fillResponse(request, response, "Error", responseMessage);
     }
 
-    const message = this.formatMessage(sender, recipient, messageType);
+    // check message
+    let messageOkay = this.checkMessage(message);
+    if (!messageOkay) {
+      let responseMessage = "We cannot send that message.";
+      return this.fillResponse(request, response, "Error", responseMessage);
+    }
+
+    const formattedMessage = this.formatMessage(sender, recipient, message);
     
     try {
-      const messageObject = messageQueue.addMessage(request.sessionId, message, date, time);
+      const messageObject = this.messageQueue.adddRequest(request.sessionId, {sender, recipient, message}, date, time);
    
-      // let responseMessage = `*** We are currently testing messages so your message NOT be display. Try this in a few days. Watch for your message "${message}".`
-      let responseMessage = `Watch for your message "${message}"`;
+      // let responseMessage = `*** We are currently testing messages so your message will NOT be display. Try this in a few days. Watch for your message "${message}".`
+      let responseMessage = `Watch for your message "${formattedMessage}"`;
       if (date != null && date != undefined && date.length > 0) {
         responseMessage += ` on ${messageObject.formattedDate}`;
       }
@@ -126,7 +157,7 @@ class MessageScene {
 
   //////////////////////////////////////////////////////////////////////////////
   // we should check the message to prevent hackers from displaying
-  // "unauthroized" messages
+  // "unauthoriized" messages
   
   checkMessage(message) {
     // to do
@@ -138,10 +169,40 @@ class MessageScene {
     return response.json({
       status: status,
       message: message,
-      source: 'NameManager'
+      source: 'MessageScene'
     });
   }
 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+function onTimer(scene) {
+  const nowTime = Date.now();
+  if (scene.startTime + scene.period <= nowTime) {
+    scene.pause();
+    return;
+  }
+
+  if (scene.getActiveMessageCount() < 1) {
+    scene.pause();
+    return;
+  }
+ 
+  console.log("messageScene onTimer");
+
+  const messageObject = scene.messageQueue.getNextRequest();
+  if (messageObject == null) {
+    scene.pause();
+    return;
+  }
+
+  // redraw every time to be safe
+  let frameBuffer = BitmapBuffer.fromNew(168, 36, new Color(0, 0, 0));
+  frameBuffer.print3Lines("To:" + messageObject.recipient, messageObject.message, "From:" + messageObject.sender, BitmapBuffer.LITTERA_WHITE_16);
+  scene.gridzilla.transformScreen(frameBuffer);
+
+  scene.runningTimer = setTimeout(onTimer, 1000, scene); 
 }
 
 module.exports = MessageScene;
