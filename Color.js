f1e7d48a7364fa30db0adab6fa5b7d23af4cbe41
Chris Maloney <cmaloney@euronetworldwide.com>
2018-12-05 23:18:24 -0600

    added more constructors

--- Color.js
+++ Color.js
@@ -2,10 +2,30 @@
 
 class Color {
   constructor(red, green, blue, alpha = 255) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
-    this.alpha = 255;
+    if (Array.isArray(red)) {
+      this.red = red[0];
+      this.green = red[1];
+      this.blue = red[2];
+      if (red.length > 3) {
+        this.alpha = red[3];
+      } else {
+        this.alpha = 255;
+      }
+    } else if (typeof red == "object") {
+      this.red = red.red;
+      this.green = red.green;
+      this.blue = red.blue;
+      if (red.alpha) {
+        this.alpha = red.alpha;
+      } else {
+        this.alpha = 255;
+      }
+    } else {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = 255;
+    }
   }
 
   get r() { return this.red; }
@@ -14,4 +34,4 @@ class Color {
   get a() { return this.alpha; }
 }
 
-module.exports = Color;
+module.exports = Color; 
