f7b60a214ce7790ddaa156bf00f159e8e0c54d72
Blake Stewart <blakemstewart@gmail.com>
2018-12-11 12:54:08 -0600

    Added the messages list

---
+++ config-messages.js
@@ -0,0 +1,42 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// colors
+//////////////////////////////////////////////////////////////////////////////
+
+const messages = [
+  "Happy Holidays",
+  "Seasons Greetings",
+  "Love, Peace, and Joy",
+  "I Love you",
+  "We Love you",
+  "Merry Christmas",
+  "Feliz Navidad",
+  "Happy Hanukkah",
+  "Shalom",
+  "Joyous Kwanzaa",
+  "Will you marry me?",
+  "Live Long and Prosper",
+  "Happy Festivus",
+  "Happy New Year",
+  "You are my BFF"];
+
+
+
+  // "Happy Holidays": "Happy Holidays",
+  // "Seasons Greetings": "Seasons Greetings",
+  // "Love, Peace, and Joy": "Love, Peace, and Joy",
+  // "I Love you": "I Love you",
+  // "We Love you": "We Love you",
+  // "Merry Christmas": "Merry Christmas",
+  // "Feliz Navidad": "Feliz Navidad",
+  // "Happy Hanukkah": "Happy Hanukkah",
+  // "Shalom": "Shalom",
+  // "Joyous Kwanzaa": "Joyous Kwanzaa",
+  // "Will you marry me?": "Will you marry me?",
+  // "Live Long and Prosper": "Live Long and Prosper",
+  // "Happy Festivus": "Happy Festivus",
+  // "Happy New Year": "Happy New Year",
+  // "You are my BFF": "You are my BFF"
+
+
+module.exports = { messages };
